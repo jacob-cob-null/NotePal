c9878c3539077d3a3c60b8d98b2d68c4d648eea1
Jacob <silveriolance06@gmail.com>
2025-06-16 13:47:53 +0800

    Made MPA structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [tailwindcss()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'), // root index.html
+        login: path.resolve(__dirname, 'src/login/login.html'),
+        dashboard: path.resolve(__dirname, 'src/dashboard/dashboard.html'),
+      },
+    },
+  },
+});
