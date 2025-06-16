3cbbc99bed79559b9f113cb17d009be06fa5504b
Jacob <silveriolance06@gmail.com>
2025-06-16 18:15:37 +0800

    added firebase authentication, and trial ui for login html

--- src/login/login.js
+++ src/login/login.js
@@ -1,1 +1,38 @@
 import '../style.css';
+
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../Firebase/setup.js'; // ✅ Import the auth instance
+
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      window.location.href = '../dashboard/dashboard.html';
+      alert('Logged in!');
+    })
+    .catch((error) => {
+      alert(`Login failed: ${error.message}`);
+    });
+});
+
+//create account
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
