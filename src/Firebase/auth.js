d4d7c161e1b7ce9f0e333515b5e5ce0e43e7ac45
unknown <silveriolance06@gmail.com>
2025-06-18 19:38:05 +0800

    Added account creation logic

--- src/Firebase/auth.js
+++ src/Firebase/auth.js
@@ -1,8 +1,25 @@
 // src/auth/auth.js
 import '../style.css';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 import { auth } from '../Firebase/setup.js';
 
 export async function loginWithEmail(email, password) {
   return await signInWithEmailAndPassword(auth, email, password);
 }
+
+export async function createAccount(email, password) {
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      return user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      throw error;
+    });
+}
+
